4cfd280047d4889d36ec422ff4f716e9166ce034
Colin Martin <Cmartin@colins-macbook-pro.local>
2016-08-03 11:16:52 -0700

    Added table and editing filter screen. Should be done

---
+++ AeroSpec/PreviousScanViewController.swift
@@ -0,0 +1,89 @@
+//
+//  PreviousScanViewController.swift
+//  AeroSpec
+//
+//  Created by Colin Martin on 8/3/16.
+//  Copyright © 2016 nrg. All rights reserved.
+//
+
+import UIKit
+
+class PreviousScanViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    
+    
+    @IBOutlet weak var check1: UIImageView!
+    @IBOutlet weak var check2: UIImageView!
+    @IBOutlet weak var check3: UIImageView!
+    @IBOutlet weak var check4: UIImageView!
+    @IBOutlet weak var check5: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        check1.hidden = true;
+        check2.hidden = true;
+        check3.hidden = true;
+        check4.hidden = true;
+        check5.hidden = true;
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func hit1(sender: AnyObject) {
+        check1.hidden = false;
+        check2.hidden = true;
+        check3.hidden = true;
+        check4.hidden = true;
+        check5.hidden = true;
+    }
+    @IBAction func hit2(sender: AnyObject) {
+        check1.hidden = true;
+        check2.hidden = false;
+        check3.hidden = true;
+        check4.hidden = true;
+        check5.hidden = true;
+    }
+    @IBAction func hit3(sender: AnyObject) {
+        check1.hidden = true;
+        check2.hidden = true;
+        check3.hidden = false;
+        check4.hidden = true;
+        check5.hidden = true;
+    }
+    @IBAction func hit4(sender: AnyObject) {
+        check1.hidden = true;
+        check2.hidden = true;
+        check3.hidden = true;
+        check4.hidden = false;
+        check5.hidden = true;
+    }
+    @IBAction func hit5(sender: AnyObject) {
+        check1.hidden = true;
+        check2.hidden = true;
+        check3.hidden = true;
+        check4.hidden = true;
+        check5.hidden = false;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AeroSpec/ScanTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScanTableViewCell.swift
+//  AeroSpec
+//
+//  Created by Colin Martin on 8/3/16.
+//  Copyright © 2016 nrg. All rights reserved.
+//
+
+import UIKit
+
+class ScanTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
